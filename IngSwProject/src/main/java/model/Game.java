78b10c1756422e0fd53e6877a3b253f6793bfe3b
MartyQ17 <116437056+MartyQ17@users.noreply.github.com>
2023-03-14 10:38:11 +0100

    Add files via upload
    First push, it should contain the basic folders

---
+++ IngSwProject/src/main/java/model/Game.java
@@ -0,0 +1,55 @@
+package model;
+
+import jdk.jshell.spi.ExecutionControl;
+//import sun.reflect.generics.reflectiveObjects.NotImplementedException; -> control the type of exception in all code
+
+public class Game {
+
+    private int numberOfPlayers;
+    private Player playerInTurn;
+    private Player firstPlayerToFinish;
+    private boolean isLastTurn;
+
+
+    public void assignPersonalCards() {
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+        } ;
+    public void chooseCommonCards(){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public void fillBoard(){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public void setNumberOfPlayers(int){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public void setNumberOfCells(){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public Player setFirstPlayer() {
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public void setWinner(){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public void endOfGame(){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public void checkBoard(){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public void refillBag(){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public void shuffleBag(){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public boolean checkIfFirstToFinish(Player){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+    public void lastTurn(){
+        try{/*code*/} catch (Exception ex) {System.out.println("Method not yet implemented");throw new ExecutionControl.NotImplementedException();}
+    } ;
+
+}
+
