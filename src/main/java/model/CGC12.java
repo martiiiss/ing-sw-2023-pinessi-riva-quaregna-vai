f4035274cc211a37e7dfdee1e65fc2530ae1da87
MartyQ17 <martina.quaregna.17@gmail.com>
2023-04-02 21:43:09 +0200

    CGC12 with its test

--- src/main/java/model/CGC12.java
+++ src/main/java/model/CGC12.java
@@ -5,43 +5,38 @@ import static model.Type.NOTHING;
 // The diagonal, both sides
 public class CGC12 implements CGMStrategy {
     public boolean compareRule(Bookshelf bks, int id) {
-
         Tile[][] bookshelf = bks.getBookshelf();
-        int i, j;
-        boolean result = false;
-        int flag = 0; //the tot of NOTHING tiles has to be 15
+        boolean flag = false;
 
-        //this controls through the matrix from top to bottom and from left to right
-        for (i = 6; i > 1; i--) {
-            for (j = 0; j < 5; j++) {
-                if (bookshelf[i][j].getType() == NOTHING) {
-                    flag++;
-                }
-            }
-        }
-        if (flag < 15) { //the if block is there because we want to control the matrix in both ways separately and then put them in OR
-            flag = 0;
-            //this controls through the matrix from top to bottom and from right to left
-            for (i = 6; i > 1; i--) {
-                for (j = 5; j > 0; j--) {
-                    if (bookshelf[i][j].getType() == NOTHING) {
-                        flag++;
-                    }
-                }
-            }
-        }
+        //check first whole diagonal
+        if ((bookshelf[0][0].getType() == NOTHING && bookshelf[1][0].getType() != NOTHING) &&
+                (bookshelf[1][1].getType() == NOTHING && bookshelf[2][1].getType() != NOTHING) &&
+                (bookshelf[2][2].getType() == NOTHING && bookshelf[3][2].getType() != NOTHING) &&
+                (bookshelf[3][3].getType() == NOTHING && bookshelf[4][3].getType() != NOTHING) &&
+                (bookshelf[4][4].getType() == NOTHING && bookshelf[5][4].getType() != NOTHING)
+            ) {flag = true;}
+        //check first whole and partial diagonal
+        if ((bookshelf[0][0].getType() != NOTHING && bookshelf[0][1].getType() == NOTHING) &&
+                (bookshelf[1][1].getType() != NOTHING && bookshelf[1][2].getType() == NOTHING) &&
+                (bookshelf[2][2].getType() != NOTHING && bookshelf[2][3].getType() == NOTHING) &&
+                (bookshelf[3][3].getType() != NOTHING && bookshelf[3][4].getType() == NOTHING) &&
+                (bookshelf[4][4].getType() != NOTHING)
+            ) {flag = true;}
+        //check second whole diagonal
+        if ((bookshelf[4][0].getType() == NOTHING && bookshelf[5][0].getType() != NOTHING) &&
+           (bookshelf[3][1].getType() == NOTHING && bookshelf[4][1].getType() != NOTHING) &&
+           (bookshelf[2][2].getType() == NOTHING && bookshelf[3][2].getType() != NOTHING) &&
+           (bookshelf[1][3].getType() == NOTHING && bookshelf[2][3].getType() != NOTHING) &&
+           (bookshelf[0][4].getType() == NOTHING && bookshelf[1][4].getType() != NOTHING)
+            ) {flag = true;}
+        //check second whole and partial diagonal
+        if ((bookshelf[4][0].getType() != NOTHING && bookshelf[3][0].getType() == NOTHING) &&
+            (bookshelf[3][1].getType() != NOTHING && bookshelf[2][1].getType() == NOTHING) &&
+            (bookshelf[2][2].getType() != NOTHING && bookshelf[1][2].getType() == NOTHING) &&
+            (bookshelf[1][3].getType() != NOTHING && bookshelf[0][3].getType() == NOTHING) &&
+            (bookshelf[0][4].getType() != NOTHING)
+        ) {flag = true;}
 
-
-        //this sets the result looking at the times the rest of the code
-        // found a NOTHING tile -> in order for this to work it has to find 15 NOTHING tiles in one control or the other
-
-        if (flag == 15) {
-            result = true;
-        } else {
-            result = false;
-        }
-        return result;
-        //try{/*code*/ throw new ExecutionControl.NotImplementedException("Method not yet implemented");} catch (Exception ex) {System.out.println("Method not yet implemented");}
-
+        return flag;
     }
 }
---
+++ src/test/java/Model/CGC12Test.java
@@ -0,0 +1,90 @@
+package Model;
+
+import model.Bookshelf;
+import model.CGC12;
+import model.Tile;
+import model.Type;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CGC12Test {
+
+    Tile cat = new Tile(Type.CAT);
+    Tile book = new Tile(Type.BOOK);
+    Tile game = new Tile(Type.GAME);
+    Tile frame = new Tile(Type.FRAME);
+    Tile trophy = new Tile(Type.TROPHY);
+    Tile plant = new Tile(Type.PLANT);
+    Tile nothing = new Tile(Type.NOTHING);
+
+    /**
+     * First test, should be TRUE -> Right to left, with first row empty
+     */
+    Tile[][] bookshelfT1 = {
+            {nothing, nothing, nothing, nothing, nothing},
+            {cat, nothing, nothing, nothing, nothing},
+            {plant, cat, nothing, nothing, nothing,},
+            {frame, trophy, cat, nothing, nothing,},
+            {frame, frame, plant, cat, nothing,},
+            {cat, plant, trophy, cat, plant}
+    };
+
+    /**
+     * Second test, should be FALSE
+     */
+    Tile[][] bookshelfF1 = {
+            {nothing, nothing, nothing, nothing, nothing},
+            {cat,     nothing, nothing, nothing, nothing},
+            {plant,   cat,     nothing, nothing, nothing,},
+            {frame,   trophy,  cat,     nothing, nothing,},
+            {frame,   frame,   plant,   cat,     cat,},
+            {cat,     plant,   trophy,  cat,     plant}
+
+    };
+
+    /**
+     * Third test, should be TRUE -> Left to right, with first row not empty
+     */
+    Tile[][] bookshelfT2 = {
+            {nothing, nothing, nothing, nothing, cat},
+            {nothing, nothing, nothing, cat, plant},
+            {nothing, nothing, cat, plant, frame,},
+            {nothing, trophy, cat, trophy, cat,},
+            {frame, frame, plant, cat, trophy,},
+            {cat, plant, trophy, cat, plant}
+    };
+
+    Tile[][] bookshelfF2 = {
+            {nothing, nothing, nothing, plant, cat},
+            {nothing, nothing, nothing, cat, plant},
+            {nothing, nothing, cat, plant, frame,},
+            {nothing, trophy, cat, trophy, cat,},
+            {frame, frame, plant, cat, trophy,},
+            {cat, plant, trophy, cat, plant}
+    };
+
+
+    private final CGC12 cgc12 = new CGC12();
+
+
+    @org.junit.jupiter.api.Test
+    void compareRule() {
+        Bookshelf bks1 = new Bookshelf();
+
+        bks1.setBookshelf(bookshelfT1);
+        assertTrue(cgc12.compareRule(bks1, 11));
+
+        bks1.setBookshelf(bookshelfF1);
+        assertFalse(cgc12.compareRule(bks1, 11));
+
+        bks1.setBookshelf(bookshelfT2);
+        assertTrue(cgc12.compareRule(bks1, 11));
+
+        bks1.setBookshelf(bookshelfF2);
+        assertFalse(cgc12.compareRule(bks1, 11));
+    }
+
+
+
+}
