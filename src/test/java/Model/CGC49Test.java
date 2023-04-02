17fec39f2110e8ad4118924193feadb4673d93db
Darthale <ale2013pv@gmail.com>
2023-04-02 11:47:07 +0200

    Added test for CommonGoalCard  49 and 68

---
+++ src/test/java/Model/CGC49Test.java
@@ -0,0 +1,73 @@
+package Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.*;
+import model.Tile;
+import model.Type;
+import model.CGC49;
+import model.Bookshelf;
+
+class CGC49Test {
+    Tile cat = new Tile(Type.CAT);
+    Tile book = new Tile(Type.BOOK);
+    Tile game = new Tile(Type.GAME);
+    Tile frame = new Tile(Type.FRAME);
+    Tile trophy = new Tile(Type.TROPHY);
+    Tile plant = new Tile(Type.PLANT);
+    Tile nothing = new Tile(Type.NOTHING);
+
+    Tile[][] bookshelfT1_4 = {
+            { cat, nothing, nothing, nothing, cat },
+            { plant, cat, game, cat, game},
+            { frame, trophy, cat, book, game},
+            { cat, cat, cat, cat, cat},
+            { game, cat, book, cat, cat},
+            { cat, plant, cat, plant, cat}
+    };
+
+    Tile[][] bookshelfF1_4 = {
+            { cat, cat, nothing, nothing, cat},
+            { plant, cat, game, cat, game},
+            { frame, trophy, cat, book, game},
+            { plant, trophy, cat, cat, book},
+            { frame, cat, book, frame, cat},
+            { cat, game, trophy, plant, book}
+    };
+    Tile[][] bookshelfT1_9 = {
+            { cat, trophy, nothing, nothing, cat },
+            { plant, cat, game, cat, cat},
+            { frame, trophy, cat, book, cat},
+            { cat, cat, cat, cat, cat},
+            { plant, cat, book, cat, cat},
+            { cat, trophy, cat, plant, cat}
+    };
+
+    Tile[][] bookshelfF1_9 = {
+            {cat, nothing, nothing, nothing, cat},
+            {plant, cat, cat, cat, game},
+            {cat, trophy, cat, cat, game},
+            {plant, trophy, cat, cat, cat},
+            {cat, cat, cat, cat, cat},
+            {cat, game, nothing, nothing, cat}
+    };
+
+    private final CGC49 cgc49 = new CGC49();
+    @org.junit.jupiter.api.Test
+    void compareRule() {
+        Bookshelf bks1 = new Bookshelf();
+
+        bks1.setBookshelf(bookshelfT1_4);
+        assertTrue(cgc49.compareRule(bks1, 4));
+
+        bks1.setBookshelf(bookshelfF1_4);
+        assertFalse(cgc49.compareRule(bks1, 4));
+
+        bks1.setBookshelf(bookshelfT1_9);
+        assertTrue(cgc49.compareRule(bks1, 9));
+
+        bks1.setBookshelf(bookshelfF1_9);
+        assertFalse(cgc49.compareRule(bks1, 9));
+    }
+
+}
