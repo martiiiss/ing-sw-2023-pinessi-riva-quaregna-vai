8300f8cf5a3bf75c730e0f24d644826fbf0a3de2
MartyQ17 <martina.quaregna.17@gmail.com>
2023-04-03 20:27:09 +0200

    test commit to see if CGC5 appears

--- src/main/java/model/CGC5.java
+++ src/main/java/model/CGC5.java
@@ -4,6 +4,13 @@ import jdk.jshell.spi.ExecutionControl;
 
  public class CGC5 implements CGMStrategy {
     public boolean compareRule(Bookshelf bks, int id){
+
+
+
+
+
+        //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
         try{/*code*/ throw new ExecutionControl.NotImplementedException("Method not yet implemented");} catch (Exception ex) {System.out.println("Method not yet implemented");}
         return false;
     }
