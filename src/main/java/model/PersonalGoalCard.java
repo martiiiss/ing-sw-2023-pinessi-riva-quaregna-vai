280527b3507195f0c551cc5ebd798b82f65824d5
MartyQ17 <martina.quaregna.17@gmail.com>
2023-04-04 17:18:34 +0200

    added readerJson method in PGC

--- src/main/java/model/PersonalGoalCard.java
+++ src/main/java/model/PersonalGoalCard.java
@@ -1,33 +1,84 @@
 package model;
-import jdk.jshell.spi.ExecutionControl;
-/*
+import com.google.gson.stream.JsonReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.util.Iterator;
-import java.lang.reflect.Type;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import com.google.gson.*;
- */
-
-
 
 
 public class PersonalGoalCard {
 
     private Type[][] seed; //tiles position
     private boolean isCompleted;
+    private int position[][];
+    private Type tileType[];
 
-    public PersonalGoalCard (int id){
+    public PersonalGoalCard (int numberOfPGC){
+
+        for(int i=0;i<5;i++){
+            this.tileType[i]= Type.NOTHING;
+            for(int j=0;j<2;j++){
+                this.position[i][j] = 0;
+            }
+        }
+
     }
 
-    //Do we really need the seed?
-    public Type[][] getSeed() {
-        try{/*code*/ throw new ExecutionControl.NotImplementedException("Method not yet implemented");} catch (Exception ex) {System.out.println("Method not yet implemented");}
-        return seed;
+    int i = 0;
+    int j = 0;
+    public void readerJSON (int id){
+        try {
+            JsonReader reader = new JsonReader(new FileReader(id +".json"));
+
+            reader.beginArray();
+            while (reader.hasNext()) {
+
+                reader.beginObject();
+                while (reader.hasNext()) {
+
+                    String parameter = reader.nextName();
+
+                    if (parameter.equals("x")) {
+                        position[i][j] = reader.nextInt();
+                        j++;
+                    } else if (parameter.equals("y")) {
+                        position[i][j] = reader.nextInt();
+                        j--;
+                    } else if (parameter.equals("type")) {
+                        switch (reader.nextString()){
+                            case "CAT": {
+                                tileType[i] = Type.CAT;
+                            }
+                            case "BOOK": {
+                                tileType[i] = Type.BOOK;
+                            }
+                            case "GAME": {
+                                tileType[i] = Type.GAME;
+                            }
+                            case "FRAME": {
+                                tileType[i] = Type.FRAME;
+                            }
+                            case "TROPHY": {
+                                tileType[i] = Type.TROPHY;
+                            }
+                            case "PLANT": {
+                                tileType[i] = Type.PLANT;
+                            }
+                        }
+                    }else {
+                        reader.skipValue();
+                    }
+                }
+                reader.endObject();
+            }
+            reader.endArray();
+
+            reader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
