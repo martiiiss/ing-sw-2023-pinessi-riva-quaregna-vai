06ac4fb83c7253cfae6befe95f69f8ef6416217e
AndreaPinessi <andrea.pinessi@mail.polimi.it>
2023-04-06 15:13:00 +0200

    Reworked the PersonalGoalCard by using a matrix instead of 2 matrices and an array. Also remade the tests for this class.

--- src/main/java/model/PersonalGoalCard.java
+++ src/main/java/model/PersonalGoalCard.java
@@ -1,64 +1,80 @@
 package model;
 import com.google.gson.stream.JsonReader;
+
+import javax.swing.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Spliterator;
 
 
 public class PersonalGoalCard {
     private boolean isCompleted;
-    private int position[][];
-    private Type tileType[];
+    private Tile[][] seedOfCard;
 
+    //numOfPGC is the "name of the card that has to be generated" this number is extracted in game by Rand(). It has to be
+    //passed as a param since it needs to be different from all the previous and next generated numOfCards (Every player must
+    //have a different card.
     public PersonalGoalCard (int numberOfPGC){
-        this.tileType = new Type[6];
-        this.position = new int[6][2];
-        for(int i=0;i<6;i++){
-            tileType[i]= Type.NOTHING;
-            for(int j=0;j<2;j++){
-                position[i][j] = 0;
-            }
-        }
+        this.seedOfCard = new Tile[6][5];
+        Tile mockTile = new Tile(Type.NOTHING,1);
+        for(int i=0;i<6;i++)
+            for(int j=0;j<5;j++)
+                this.seedOfCard[i][j] = mockTile;
         readerJSON(numberOfPGC);
     }
-    int i = 0;
-    int j = 0;
-    public void readerJSON (int id){
+    public void readerJSON (int id) { //To be set as private once the tests are done
+        int i = 0, j = 0;
+        Tile readTile;
         try {
-            JsonReader reader = new JsonReader(new FileReader("src/main/java/model/PersonalGoalCardDescription/"+id+".json"));
-
+            JsonReader reader = new JsonReader(new FileReader("src/main/java/model/PersonalGoalCardDescription/" + id + ".json"));
             reader.beginArray();
             while (reader.hasNext()) {
                 reader.beginObject();
                 while (reader.hasNext()) {
-                    String parameter = reader.nextName();
-                    if (parameter.equals("x")) {
-                        position[i][j] = reader.nextInt();
-                        j++;
-                    } else if (parameter.equals("y")) {
-                        position[i][j] = reader.nextInt();
-                        j--;
-                    } else if (parameter.equals("type")) {
-                        switch (reader.nextString()){
-                            case "CAT"    -> tileType[i] = Type.CAT;
-                            case "BOOK"   -> tileType[i] = Type.BOOK;
-                            case "GAME"   -> tileType[i] = Type.GAME;
-                            case "FRAME"  -> tileType[i] = Type.FRAME;
-                            case "TROPHY" -> tileType[i] = Type.TROPHY;
-                            case "PLANT"  -> tileType[i] = Type.PLANT;
-                            default -> throw new IllegalStateException("Unexpected value:"+ reader.nextString());
+                    String row = reader.nextName();
+                    if (row.equals("x")) {
+                        i = reader.nextInt();
+                        System.out.print(i+" ");
+                    }
+                    String col = reader.nextName();
+                    if (col.equals("y")) {
+                        j = reader.nextInt();
+                        System.out.print(j+" ");
+                    }
+                    String type = reader.nextName();
+                    if (type.equals("type")) {
+                        switch (reader.nextString()) {
+                            case "CAT" -> readTile = new Tile(Type.CAT,0);
+                            case "BOOK" -> readTile = new Tile(Type.BOOK,0);
+                            case "GAME" -> readTile = new Tile(Type.GAME,0);
+                            case "FRAME" -> readTile = new Tile(Type.FRAME,0);
+                            case "TROPHY" -> readTile = new Tile(Type.TROPHY,0);
+                            case "PLANT" -> readTile = new Tile(Type.PLANT,0);
+                            default -> throw new IllegalStateException("Unexpected value:" + reader.nextString());
                         }
-                        i++;
-                    }else{reader.skipValue();}
+                        seedOfCard[i][j]=readTile;
+                        System.out.println(seedOfCard[i][j].getType());
+                    } else {
+                        reader.skipValue();
+                    }
                 }
                 reader.endObject();
             }
             reader.endArray();
             reader.close();
-        } catch (FileNotFoundException e) {e.printStackTrace();}
-          catch (IOException e) {e.printStackTrace();}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for(i=0;i<6;i++) {
+            System.out.println();
+            for(j=0;j<5;j++)
+                System.out.print(this.seedOfCard[i][j].getType()+" ");
+        }
     }
-    public int[][] getPosition(){return this.position;}
-    public Type[] getTileType(){return this.tileType;}
+
+    public Tile[][] getPGC() {
+        return this.seedOfCard;
+    }
 
 }
--- src/test/java/Model/PersonalGoalCardTest.java
+++ src/test/java/Model/PersonalGoalCardTest.java
@@ -1,95 +1,138 @@
 package Model;
 
+import model.Tile;
 import org.junit.jupiter.api.Test;
 import model.PersonalGoalCard;
 import model.Type;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 class PersonalGoalCardTest {
-
-    final PersonalGoalCard pgc1 = new PersonalGoalCard(1);
-    final PersonalGoalCard pgc2 = new PersonalGoalCard(2);
-    final PersonalGoalCard pgc3 = new PersonalGoalCard(3);
-    final PersonalGoalCard pgc4 = new PersonalGoalCard(4);
-    final PersonalGoalCard pgc5 = new PersonalGoalCard(5);
-    final PersonalGoalCard pgc6 = new PersonalGoalCard(6);
-    final PersonalGoalCard pgc7 = new PersonalGoalCard(7);
-    final PersonalGoalCard pgc8 = new PersonalGoalCard(8);
-    final PersonalGoalCard pgc9 = new PersonalGoalCard(9);
-    final PersonalGoalCard pgc10 = new PersonalGoalCard(10);
-    final PersonalGoalCard pgc11 = new PersonalGoalCard(11);
-    final PersonalGoalCard pgc12 = new PersonalGoalCard(12);
-
     @Test
     void readerJSONTest() {
-        int[][] card1 = {{0,0},{0,2},{1,4},{2,3},{3,1},{5,2}}; //position
-        Type[] tileType1 = {Type.PLANT, Type.FRAME, Type.CAT, Type.BOOK, Type.GAME, Type.TROPHY};
+        PersonalGoalCard personalGoalCard1 = new PersonalGoalCard(1);
+        PersonalGoalCard personalGoalCard2 = new PersonalGoalCard(2);
+        PersonalGoalCard personalGoalCard3 = new PersonalGoalCard(3);
+        PersonalGoalCard personalGoalCard4 = new PersonalGoalCard(4);
+        PersonalGoalCard personalGoalCard5 = new PersonalGoalCard(5);
+        PersonalGoalCard personalGoalCard6 = new PersonalGoalCard(6);
+        PersonalGoalCard personalGoalCard7 = new PersonalGoalCard(7);
+        PersonalGoalCard personalGoalCard8 = new PersonalGoalCard(8);
+        PersonalGoalCard personalGoalCard9 = new PersonalGoalCard(9);
+        PersonalGoalCard personalGoalCard10 = new PersonalGoalCard(10);
 
-        int[][] card2 = {{1,1},{2,0},{2,2},{3,4},{4,3},{5,4}}; //position
-        Type[] tileType2 = {Type.PLANT, Type.CAT, Type.GAME, Type.BOOK, Type.TROPHY, Type.FRAME};
+        Tile cat = new Tile(Type.CAT, 1);
+        Tile book = new Tile(Type.BOOK, 2);
+        Tile game = new Tile(Type.GAME, 3);
+        Tile frame = new Tile(Type.FRAME, 2);
+        Tile trophy = new Tile(Type.TROPHY, 1);
+        Tile plant = new Tile(Type.PLANT, 3);
+        Tile nothing = new Tile(Type.NOTHING, 0);
 
-        int[][] card3 = {{1,0},{1, 3},{2,2},{3,1},{3,4},{5,0}}; //position
-        Type[] tileType3 = {Type.FRAME, Type.GAME, Type.PLANT, Type.CAT, Type.TROPHY, Type.BOOK};
+        Tile[][] pgc1 = personalGoalCard1.getPGC();
+        Tile[][] pgc2 = personalGoalCard2.getPGC();
+        Tile[][] pgc3 = personalGoalCard3.getPGC();
+        Tile[][] pgc4 = personalGoalCard4.getPGC();
+        Tile[][] pgc5 = personalGoalCard5.getPGC();
+        Tile[][] pgc6 = personalGoalCard6.getPGC();
+        Tile[][] pgc7 = personalGoalCard7.getPGC();
+        Tile[][] pgc8 = personalGoalCard8.getPGC();
+        Tile[][] pgc9 = personalGoalCard9.getPGC();
+        Tile[][] pgc10 = personalGoalCard10.getPGC();
 
-        int[][] card4 = {{0,4},{2,0},{2,2},{3,3},{4,1},{4,2}}; //position
-        Type[] tileType4 = {Type.GAME, Type.TROPHY, Type.FRAME, Type.PLANT, Type.BOOK, Type.CAT};
+        Tile[][] bookshelf1 = {
+                {plant, nothing, frame, nothing, nothing},
+                {nothing, nothing, nothing, nothing, cat},
+                {nothing, nothing, nothing, book, nothing},
+                {nothing, game, nothing, nothing, nothing},
+                {nothing, nothing, nothing, nothing, nothing},
+                {nothing, nothing, trophy, nothing, nothing}};
+        Tile[][] bookshelf2 = {
+                {nothing, nothing, nothing, nothing, nothing},
+                {nothing, plant, nothing, nothing, nothing},
+                {cat, nothing, game, nothing, nothing},
+                {nothing, nothing, nothing, nothing, book},
+                {nothing, nothing, nothing, trophy, nothing},
+                {nothing, nothing, nothing, nothing, frame}};
+        Tile[][] bookshelf3 = {
+                {nothing, nothing, nothing, nothing, nothing},
+                {frame, nothing, nothing, game, nothing},
+                {nothing, nothing, plant, nothing, nothing},
+                {nothing, cat, nothing, nothing, nothing},
+                {nothing, nothing, nothing, nothing, trophy},
+                {book, nothing, nothing, nothing, nothing}};
+        Tile[][] bookshelf4 = {
+                {nothing, nothing, nothing, nothing, game},
+                {nothing, nothing, nothing, nothing, nothing},
+                {trophy, nothing, frame, nothing, nothing},
+                {nothing, nothing, nothing, plant, nothing},
+                {nothing, book, cat, nothing, nothing},
+                {nothing, nothing, nothing, nothing, nothing}};
+        Tile[][] bookshelf5 = {
+                {nothing, nothing, nothing, nothing, nothing},
+                {nothing, trophy, nothing, nothing, nothing},
+                {nothing, nothing, nothing, nothing, nothing},
+                {nothing, frame, book, nothing, nothing},
+                {nothing, nothing, nothing, plant, nothing},
+                {game, nothing, nothing, cat, nothing}};
+        Tile[][] bookshelf6 = {
+                {nothing, nothing, trophy, nothing, cat},
+                {nothing, nothing, nothing, nothing, nothing},
+                {nothing, nothing, nothing, book, nothing},
+                {nothing, nothing, nothing, nothing, nothing},
+                {nothing, game, nothing, frame, nothing},
+                {plant, nothing, nothing, nothing, nothing}};
+        Tile[][] bookshelf7 = {
+                {cat, nothing, nothing, nothing, nothing},
+                {nothing, nothing, nothing, frame, nothing},
+                {nothing, plant, nothing, nothing, nothing},
+                {trophy, nothing, nothing, nothing, nothing},
+                {nothing, nothing, nothing, nothing, game},
+                {nothing, nothing, book, nothing, nothing}};
+        Tile[][] bookshelf8 = {
+                {nothing, nothing, nothing, nothing, frame},
+                {nothing, cat, nothing, nothing, nothing},
+                {nothing, nothing, trophy, nothing, nothing},
+                {plant, nothing, nothing, nothing, nothing},
+                {nothing, nothing, nothing, book, nothing},
+                {nothing, nothing, nothing, game, nothing}};
+        Tile[][] bookshelf9 = {
+                {nothing, nothing, game, nothing, nothing},
+                {nothing, nothing, nothing, nothing, nothing},
+                {nothing, nothing, cat, nothing, nothing},
+                {nothing, nothing, nothing, nothing, book},
+                {nothing, trophy, nothing, nothing, plant},
+                {frame, nothing, nothing, nothing, nothing}};
+        Tile[][] bookshelf10 = {
+                {nothing, nothing, nothing, nothing, trophy},
+                {nothing, game, nothing, nothing, nothing},
+                {book, nothing, nothing, nothing, nothing},
+                {nothing, nothing, nothing, cat, nothing},
+                {nothing, frame, nothing, nothing, nothing},
+                {nothing, nothing, nothing, plant, nothing}};
+        Tile[][] bookshelfNot = {
+                {plant, nothing, nothing, frame, nothing},
+                {nothing, nothing, nothing, nothing, cat},
+                {nothing, nothing, nothing, book, nothing},
+                {nothing, plant, nothing, nothing, nothing},
+                {nothing, nothing, nothing, nothing, nothing},
+                {nothing, nothing, trophy, nothing, nothing}};
 
-        int[][] card5 = {{1,1},{3,1},{3,2},{4,4},{5,0},{5,3}}; //position
-        Type[] tileType5 = {Type.TROPHY, Type.FRAME, Type.BOOK, Type.PLANT, Type.GAME, Type.CAT};
-
-        int[][] card6 = {{0,2},{0,4},{2,3},{4,1},{4,3},{5,0}}; //position
-        Type[] tileType6 = {Type.TROPHY, Type.CAT, Type.BOOK, Type.GAME, Type.FRAME, Type.PLANT};
-
-        int[][] card7 = {{0,0},{1,3},{2,1},{3,0},{4,4},{5,2}}; //position
-        Type[] tileType7 = {Type.CAT, Type.FRAME, Type.PLANT, Type.TROPHY, Type.GAME, Type.BOOK};
-
-        int[][] card8 = {{0,4},{1,1},{2,2},{3,0},{4,3},{5,3}}; //position
-        Type[] tileType8 = {Type.FRAME, Type.CAT, Type.TROPHY, Type.PLANT, Type.BOOK, Type.GAME};
-
-        int[][] card9 = {{0,2},{2,2},{3,4},{4,1},{4,4},{5,0}}; //position
-        Type[] tileType9 = {Type.GAME, Type.CAT, Type.BOOK, Type.TROPHY, Type.PLANT, Type.FRAME};
-
-        int[][] card10 = {{0,4},{1,1},{2,0},{3,3},{4,1},{5,3}}; //position
-        Type[] tileType10 = {Type.TROPHY, Type.GAME, Type.BOOK, Type.CAT, Type.FRAME, Type.PLANT};
-
-        int[][] card11 = {{0,2},{1,1},{2,0},{3,2},{4,4},{5,3}}; //position
-        Type[] tileType11 = {Type.PLANT, Type.BOOK, Type.GAME, Type.FRAME, Type.CAT, Type.TROPHY};
-
-        int[][] card12 = {{0,2},{1,1},{2,2},{3,3},{4,4},{5,0}}; //position
-        Type[] tileType12 = {Type.BOOK, Type.PLANT, Type.FRAME, Type.TROPHY, Type.GAME, Type.CAT};
-
-
-
-        for(int r=0; r<6; r++){
-            for(int c=0; c<2; c++){
-                assertEquals(card1[r][c], pgc1.getPosition()[r][c]);
-                assertEquals(card2[r][c], pgc2.getPosition()[r][c]);
-                assertEquals(card3[r][c], pgc3.getPosition()[r][c]);
-                assertEquals(card4[r][c], pgc4.getPosition()[r][c]);
-                assertEquals(card5[r][c], pgc5.getPosition()[r][c]);
-                assertEquals(card6[r][c], pgc6.getPosition()[r][c]);
-                assertEquals(card7[r][c], pgc7.getPosition()[r][c]);
-                assertEquals(card8[r][c], pgc8.getPosition()[r][c]);
-                assertEquals(card9[r][c], pgc9.getPosition()[r][c]);
-                assertEquals(card10[r][c], pgc10.getPosition()[r][c]);
-                assertEquals(card11[r][c], pgc11.getPosition()[r][c]);
-                assertEquals(card12[r][c], pgc12.getPosition()[r][c]);
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 2; j++) {
+                assertEquals(bookshelf1[i][j].getType(), pgc1[i][j].getType());
+                assertEquals(bookshelf2[i][j].getType(), pgc2[i][j].getType());
+                assertEquals(bookshelf3[i][j].getType(), pgc3[i][j].getType());
+                assertEquals(bookshelf4[i][j].getType(), pgc4[i][j].getType());
+                assertEquals(bookshelf5[i][j].getType(), pgc5[i][j].getType());
+                assertEquals(bookshelf6[i][j].getType(), pgc6[i][j].getType());
+                assertEquals(bookshelf7[i][j].getType(), pgc7[i][j].getType());
+                assertEquals(bookshelf8[i][j].getType(), pgc8[i][j].getType());
+                assertEquals(bookshelf9[i][j].getType(), pgc9[i][j].getType());
+                assertEquals(bookshelf10[i][j].getType(), pgc10[i][j].getType());
+                assertNotSame(bookshelfNot[i][j],pgc1[i][j].getType());
             }
-        }
-        for(int i=0; i<6; i++){
-            assertEquals(tileType1[i], pgc1.getTileType()[i]);
-            assertEquals(tileType2[i], pgc2.getTileType()[i]);
-            assertEquals(tileType3[i], pgc3.getTileType()[i]);
-            assertEquals(tileType4[i], pgc4.getTileType()[i]);
-            assertEquals(tileType5[i], pgc5.getTileType()[i]);
-            assertEquals(tileType6[i], pgc6.getTileType()[i]);
-            assertEquals(tileType7[i], pgc7.getTileType()[i]);
-            assertEquals(tileType8[i], pgc8.getTileType()[i]);
-            assertEquals(tileType9[i], pgc9.getTileType()[i]);
-            assertEquals(tileType10[i], pgc10.getTileType()[i]);
-            assertEquals(tileType11[i], pgc11.getTileType()[i]);
-            assertEquals(tileType12[i], pgc12.getTileType()[i]);
         }
     }
 }
