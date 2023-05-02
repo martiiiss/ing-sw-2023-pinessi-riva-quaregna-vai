2689f5412dbbd31d116b16ac27bc68adeb11b862
AndreaPinessi <andrea.pinessi@mail.polimi.it>
2023-05-02 18:09:55 +0200

    UserInterface, changed the colors in the print of the Board

--- src/main/java/view/UserInterface.java
+++ src/main/java/view/UserInterface.java
@@ -186,13 +186,14 @@ public class UserInterface extends Observable implements Observer {
             System.out.print(+i+" ");
             for (int j = 0; j < board.BOARD_COLUMN; j++) {
                 switch (tilesOnBoard[i][j].getType()) {
-                    case NOTHING, BLOCKED -> System.out.print("\033[31;47;1m   \u001B[0m");
+                    case BLOCKED -> System.out.print("\033[31;47;1m   \u001B[0m");
                     case CAT -> System.out.print("\u001B[32m □ \u001B[0m");
                     case BOOK -> System.out.print("\u001B[97m □ \u001B[0m");
                     case FRAME -> System.out.print("\u001B[34m □ \u001B[0m");
                     case GAME -> System.out.print("\u001B[33m □ \u001B[0m");
                     case PLANT -> System.out.print("\u001B[35m □ \u001B[0m");
                     case TROPHY -> System.out.print("\u001B[36m □ \u001B[0m");
+                    case NOTHING -> System.out.print("\033[31;40;0m   \u001B[0m");
                 }
             }
         }
