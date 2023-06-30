be63ddd58de03cc6f0c0b933b4cd8a79ae3477e2
MartyQ17 <martina.quaregna.17@gmail.com>
2023-06-30 11:13:05 +0200

    added documentation

---
+++ deliveries/documentation/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"view"},{"l":"view.GUI"}];updateSearchResults();
---
+++ deliveries/documentation/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"view.GUI","l":"BoardView"},{"p":"view.GUI","l":"BookshelfView"},{"p":"view.GUI","l":"CGCView"},{"p":"view.GUI","l":"GUIView"},{"p":"view.GUI","l":"HandView"},{"p":"view.GUI","l":"PGCView"},{"p":"view.GUI","l":"ScoringTokenView"},{"p":"view","l":"UserView"}];updateSearchResults();
